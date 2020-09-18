722edd1de46600069fef9e815bb9180f29691384
Rebecca <rebeccalassiter2020@u.northwestern.edu>
2020-09-17 19:09:03 -0500

    chart has titles

--- sourdough-tracker/src/index.js
+++ sourdough-tracker/src/index.js
@@ -23,23 +23,40 @@ class Page extends React.Component{
 
 	//getting initial data
 	componentDidMount() {
-        fetch('/bakes')
+		fetch('/bakes')
 					.then(response => response.json())
 					.then(data => this.setState(data))
 					.then(ans => console.log(this.state));
-    }
+	}
 
-    callbackFunction = (updatedData) => {this.setState(updatedData)};
+	callbackFunction = (updatedData) => {this.setState(updatedData)};
 	
 
 	render(){
+
+			const mapTitlesToState = {      
+					"Amount of Flour (grams)": "gramsFlour",
+					"Amount of Water (grams)": "gramsWater",
+					"Amount of Sourdough Starter (grams)": "gramsStarter",
+					"Number of Stretch and Folds": "numStretchFold",
+					"Autolyse Time (minutes)": "autolyseTime",
+					"Bulk Fermentation Time (minutes)": "bulkFermentTime",
+					"Bake Time (minutes)": "bakeTime",
+					"Overall Quality": "overallQuality",
+					"Rise": "rise",
+					"Crumb": "crumb",
+					"Crust": "crust", 
+					"Flavor":"flavor"    
+	  };
 		return(
+
+		
 			<div className="App"> 
 				<OpenForm />
 				{this.state && 
-				<Graph data={this.state}/>
+				<Graph data={this.state} mapTitlesToState = {mapTitlesToState}/>
 			}
-				<Form parentCallback = {this.callbackFunction}/>
+				<Form parentCallback = {this.callbackFunction} mapTitlesToState = {mapTitlesToState}/>
 				
 			</div> 
 		)
@@ -73,115 +90,90 @@ class Graph extends React.Component{
 	}
 
 	componentDidMount() {
+		this.drawChart(this.state['bakes'])
+	}
 
-		//console.log("data is: ", this.state['bakes'])
-        this.drawChart(this.state['bakes'])
-    }
+	getKeyByValue(object, value) {
+		return Object.keys(object).find(key => object[key] === value);
+	}
+
+	//d3 creating the actual chart
+	drawChart(data){
+
+		//these two values should be changed by user selecting from dropdown
+		const xAxisVal = 'autolyseTime'
+		const yAxisVal = 'overallQuality'
+		const xAxisTitle = this.getKeyByValue(this.props.mapTitlesToState, xAxisVal)
+		const yAxisTitle = this.getKeyByValue(this.props.mapTitlesToState, yAxisVal)
+
+		var margin = {top: 10, right: 30, bottom: 30, left: 60},
+		width = 460 - margin.left - margin.right,
+		height = 400 - margin.top - margin.bottom;
 
 
-    //d3 creating the actual chart
-    drawChart(data){
-
-    	console.log("data is ", data)
-
-    	// const scale = 20; 
-
-    	// const height = 400; 
-    	// const width = 600; 
-
-    	// const svg = d3.select(this.refs.canvas)
-					// 	    .append("svg")
-					// 	    .attr("width", 600)
-					// 	    .attr("height", 400)
-					// 	    .style("border", "1px solid black");
-
-					var margin = {top: 10, right: 30, bottom: 30, left: 60},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
-
+		//setting up canvas
 		var svg = d3.select(this.refs.canvas)
-  .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")")
+					  .append("svg")
+						.attr("width", width + margin.left + margin.right)
+						.attr("height", height + margin.top + margin.bottom)
+					  .append("g")
+						.attr("transform",
+							  "translate(" + margin.left + "," + margin.top + ")")
 
-    	// Add X axis
-		  var x = d3.scaleLinear()
-		    .domain([0, 100])
-		    .range([ 0, width ]);
-		  svg.append("g")
-		    .attr("transform", "translate(0," + height + ")")
-		    .call(d3.axisBottom(x));
+		// Add X axis
+		var x = d3.scaleLinear()
+					.domain([0, 100])
+					.range([ 0, width ]);
+				  svg.append("g")
+					.attr("transform", "translate(0," + height + ")")
+					.call(d3.axisBottom(x));
 
 		  // Add Y axis
-		  var y = d3.scaleLinear()
-		    .domain([0, 100])
-		    .range([ height, 0]);
-		  svg.append("g")
-		    .call(d3.axisLeft(y));
+		var y = d3.scaleLinear()
+					.domain([0, 100])
+					.range([ height, 0]);
+				  svg.append("g")
+					.call(d3.axisLeft(y));
+
+
+
+		// Add X axis label:
+  svg.append("text")
+	  .attr("text-anchor", "end")
+	  .attr("x", width/2 + margin.left)
+	  .attr("y", height + margin.top + 20)
+	  .text(xAxisTitle);
+
+  // Y axis label:
+  svg.append("text")
+	  .attr("text-anchor", "end")
+	  .attr("transform", "rotate(-90)")
+	  .attr("y", -margin.left + 20)
+	  .attr("x", -margin.top - height/2 + 20)
+	  .text(yAxisTitle)
 
 		  // Add dots
 		  svg.append('g')
-		    .selectAll("dot")
-		    .data(data)
-		    .enter()
-		    .append("circle")
-		      .attr("cx", function (d) { return x(d['autolyseTime']); } )
-		      .attr("cy", function (d) { return y(d['overallQuality']); } )
-		      .attr("r", 4)
-		      .style("fill", "#1f2041")
+			.selectAll("dot")
+			.data(data)
+			.enter()
+			.append("circle")
+			  .attr("cx", function (d) { return x(d[xAxisVal]); } )
+			  .attr("cy", function (d) { return y(d[yAxisVal]); } )
+			  .attr("r", 4)
+			  .style("fill", "#1f2041");
 
-    	
-
-
-		// //each datapoint is a different bake entry
-	 //    svgCanvas.selectAll("rect")
-		// 	    .data(data).enter()
-		// 	         .append("rect")
-		// 	         .attr("width", 40)
-		// 	         .attr("height", (datapoint) => datapoint['autolyseTime'] * scale)
-		// 	         .attr("fill", "orange")
-		// 	         .attr("x", (datapoint, iteration) => iteration * 45)
-  //       			 .attr("y", (datapoint) => 400 - datapoint['autolyseTime'] * scale);
+		
 
 
 
 
-    }
+	}
 
 	render(){
 
 		return <div ref="canvas"></div>
-
-		// const myData = this.state['bakes']
-
-		// //need this if else if the fetch fails for some reason or if we are passed null data
-
-		// //{myData[0]['autolyseTime']}
-		// if(myData){
-		// 	return(
-		// 			<div className="graph">
-		// 		  <p>
-		// 			Graph of Your Data Here and Data from flask: {myData.length} 
-		// 		  </p>
-				  
-		// 		</div>
-		// 		);
-		// }
-		// else{
-		// 	return (
-		// 			<div className="graph">
-		// 		  <p>
-		// 			Graph of Your Data Here and Data from flask:
-		// 		  </p>
-				  
-		// 		</div>
-		// 		);
-		//}
-		
-  }
+	}
 }
 
 class Form extends React.Component{
@@ -264,29 +256,29 @@ class Form extends React.Component{
 								 ]
 		const resultsFormEntryTitles = ["Overall Quality", "Rise", "Crumb", "Crust", "Flavor"]
 
-		const mapTitlesToState = {      
-					"Amount of Flour (grams)": "gramsFlour",
-					"Amount of Water (grams)": "gramsWater",
-					"Amount of Sourdough Starter (grams)": "gramsStarter",
-					"Number of Stretch and Folds": "numStretchFold",
-					"Autolyse Time (minutes)": "autolyseTime",
-					"Bulk Fermentation Time (minutes)": "bulkFermentTime",
-					"Bake Time (minutes)": "bakeTime",
-					"Overall Quality": "overallQuality",
-					"Rise": "rise",
-					"Crumb": "crumb",
-					"Crust": "crust", 
-					"Flavor":"flavor"    
-	  }
+		// const mapTitlesToState = {      
+		// 			"Amount of Flour (grams)": "gramsFlour",
+		// 			"Amount of Water (grams)": "gramsWater",
+		// 			"Amount of Sourdough Starter (grams)": "gramsStarter",
+		// 			"Number of Stretch and Folds": "numStretchFold",
+		// 			"Autolyse Time (minutes)": "autolyseTime",
+		// 			"Bulk Fermentation Time (minutes)": "bulkFermentTime",
+		// 			"Bake Time (minutes)": "bakeTime",
+		// 			"Overall Quality": "overallQuality",
+		// 			"Rise": "rise",
+		// 			"Crumb": "crumb",
+		// 			"Crust": "crust", 
+		// 			"Flavor":"flavor"    
+	 //  }
 
 		const processFormEntries = processFormEntryTitles.map( x =>
 		{
-			return <ProcessFormEntry title = {x} key = {x} name ={mapTitlesToState[x]} onChange = {this.myChangeHandler}/>; 
+			return <ProcessFormEntry title = {x} key = {x} name ={this.props.mapTitlesToState[x]} onChange = {this.myChangeHandler}/>; 
 		});
 
 		const resultsFormEntries = resultsFormEntryTitles.map( x =>
 		{
-			return <ResultsFormEntry title = {x} key = {x} name = {mapTitlesToState[x]} onChange = {this.myChangeHandler}/>; 
+			return <ResultsFormEntry title = {x} key = {x} name = {this.props.mapTitlesToState[x]} onChange = {this.myChangeHandler}/>; 
 		});
 
 		return(
